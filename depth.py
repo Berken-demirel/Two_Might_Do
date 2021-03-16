900eba8dd23e18c413741354e2d33879b07dd475
berkenutku <berkenutku.demirel@gmail.com>
2021-03-16 14:56:41 -0700

    Update depth.py

--- depth.py
+++ depth.py
@@ -39,12 +39,11 @@ def decay(epoch, steps=100):
 
 
 def inception_module_1(layer_in):
-    conv1 = Conv1D(32, 4, padding='same', activation='relu', kernel_initializer='GlorotNormal',kernel_regularizer=l2(0.0002))(layer_in)
-    conv4 = Conv1D(32, 8, padding='same', activation='relu', kernel_initializer='GlorotNormal',kernel_regularizer=l2(0.0002))(layer_in)
-    layer_out = concatenate([conv1, conv4], axis=-1)
+    conv4 = Conv1D(32, 4, padding='same', activation='relu', kernel_initializer='GlorotNormal',kernel_regularizer=l2(0.0002))(layer_in)
+    conv16 = Conv1D(32, 16, padding='same', activation='relu', kernel_initializer='GlorotNormal',kernel_regularizer=l2(0.0002))(layer_in)
+    layer_out = concatenate([conv4, conv16], axis=-1)
     x3 = BatchNormalization()(layer_out)
     return x3
-
 
 def res_net_block1(input_data, filters, conv_size):
     x = Conv1D(filters, conv_size, activation='relu', padding='same', kernel_regularizer=l2(0.0002))(input_data)
@@ -54,7 +53,6 @@ def res_net_block1(input_data, filters, conv_size):
     x = Add()([x, input_data])
     x = Activation('relu')(x)
     return x
-
 
 def res_net_block_trans(input_data, filters, conv_size):
     input_trans = Conv1D(filters, 1, activation='relu', padding='same', kernel_regularizer=l2(0.0002))(input_data)
@@ -68,34 +66,39 @@ def res_net_block_trans(input_data, filters, conv_size):
 
 def bpm_dense(bpm_input):
     flat = Flatten()(bpm_input)
-    Dense_bpm1 = Dense(8, activation="relu")(flat)
+    Dense_bpm1 = Dense(4, activation="relu")(flat)
     Dense_bpm2 = Dense(16, activation="relu")(Dense_bpm1)
-    Dense_bpm3 = Dense(32, activation="relu")(Dense_bpm2)
+    Dense_bpm3 = Dense(64, activation="relu")(Dense_bpm2)
     return Dense_bpm3
     
 def Lead_II_way(lead_II):
     layer_out = Conv1D(32, 8, activation='relu', kernel_initializer='GlorotNormal', padding='same',kernel_regularizer=l2(0.0002))(lead_II)
     Batch1 = BatchNormalization()(layer_out)
-    layer_out_0 = res_net_block_trans(Batch1, 32, 4)
-    Pool1 = AveragePooling1D(2, padding='same')(layer_out_0)
+    layer_out_0 = res_net_block_trans(Batch1, 32, 8)
+    layer_out_1 = res_net_block_trans(layer_out_0, 64, 4)
+    Pool1 = AveragePooling1D(2, padding='same')(layer_out_1)
     Incept_1 = inception_module_1(Pool1)
-    res2 = res_net_block_trans(Incept_1, 64, 2)
-    Pool1 = AveragePooling1D(2, padding='same')(res2)
+    res2 = res_net_block_trans(Incept_1, 64, 4)
+    res3 = res_net_block_trans(res2, 128, 2)
+    Pool1 = AveragePooling1D(2, padding='same')(res3)
     #flat = Flatten()(Pool1)
     return Pool1
 
 def Lead_V5_way(lead_V5):
     layer_out = Conv1D(32, 8, activation='relu', kernel_initializer='GlorotNormal', padding='same',kernel_regularizer=l2(0.0002))(lead_V5)
     Batch1 = BatchNormalization()(layer_out)
-    layer_out_0 = res_net_block_trans(Batch1, 32, 4)
-    Pool1 = AveragePooling1D(2, padding='same')(layer_out_0)
+    layer_out_0 = res_net_block_trans(Batch1, 32, 8)
+    layer_out_1 = res_net_block_trans(layer_out_0, 64, 4)
+    Pool1 = AveragePooling1D(2, padding='same')(layer_out_1)
     Incept_1 = inception_module_1(Pool1)
-    res2 = res_net_block_trans(Incept_1, 64, 2)
-    Pool1 = AveragePooling1D(2, padding='same')(res2)
+    res2 = res_net_block_trans(Incept_1, 64, 4)
+    res3 = res_net_block_trans(res2, 128, 2)
+    Pool1 = AveragePooling1D(2, padding='same')(res3)
+    #Pool1 = signal.decimate(res2, 2)
     #flat = Flatten()(Pool1)
     return Pool1
 
-def define_model(in_shape=(600, 1), out_shape=27):
+def define_model(in_shape=(600, 1), out_shape=24):
     input_II = Input(shape=(275,1))
     input_V5 = Input(shape=(275,1))
     input_bpm = Input(shape=(6,1))
@@ -103,7 +106,7 @@ def define_model(in_shape=(600, 1), out_shape=27):
     out_II = Lead_II_way(input_II)
     out_V5 = Lead_V5_way(input_V5)
     layer_out = concatenate([out_II, out_V5], axis=-1)
-    sep1 = SeparableConv1D(128, 2, activation='relu', kernel_initializer='GlorotNormal', padding='same',kernel_regularizer=l2(0.0002))(layer_out)
+    sep1 = SeparableConv1D(128, 4, activation='relu', kernel_initializer='GlorotNormal', padding='same',kernel_regularizer=l2(0.0002))(layer_out)
     flat = Flatten()(sep1)
     Dense_1 = Dense(128, activation='relu')(flat)
     layer_out = concatenate([Dense_bpm, Dense_1])
@@ -115,13 +118,13 @@ def define_model(in_shape=(600, 1), out_shape=27):
     opt = Adam(learning_rate=0.0003)
     #opt = SGD(lr=0.01, momentum=0.9, nesterov=False)
     BerkenLeNet.compile(optimizer=opt, loss='binary_crossentropy', metrics=['Recall', 'accuracy',
-                                                                            tfa.metrics.F1Score(num_classes=27,
+                                                                            tfa.metrics.F1Score(num_classes=24,
                                                                                                 threshold=0.5,
                                                                                                 average='macro')])
     return BerkenLeNet
 
 
-a = np.load('./berken_corr/all_with_v1.npz')
+a = np.load('./berken_corr/all_less_labels.npz')
 training_data = a['arr_0']
 labels = a['arr_1']
 
@@ -129,12 +132,12 @@ Xtrain = training_data
 
 x_train, x_val, y_train, y_val = train_test_split(Xtrain, labels, stratify=labels, test_size=0.2, random_state=1)
 
-Xtrain_1 = x_train[:,0:550]
+
 Xtrain_II = x_train[:,0:275]
 Xtrain_V5 = x_train[:,275:550]
 bpm_data_train = x_train[:,550:]
 
-xval = x_val[:,0:550]
+
 xval_II = x_val[:,0:275]
 xval_V5 = x_val[:,275:550]
 bpm_data_val = x_val[:,550:]
@@ -145,7 +148,7 @@ class_weights = assign_weigths(sum_26)
 
 model = define_model()
 
-checkpoint_filepath = 'Checkpoints_all_with_v1'
+checkpoint_filepath = 'Checkpoints_increased_params_v4'
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_filepath, save_weights_only=False,
                                                                monitor='val_f1_score', mode='max', save_best_only=True)
 
@@ -158,6 +161,6 @@ where_am_I = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_f1_score', factor
 history = model.fit(x=[Xtrain_II, Xtrain_V5, bpm_data_train], y=y_train, epochs=900, batch_size=250, verbose=1, validation_data=([xval_II, xval_V5, bpm_data_val], y_val), class_weight=class_weights, callbacks=[model_checkpoint_callback, stop_me, where_am_I])
 
 hist_df = pd.DataFrame(history.history)
-pd.DataFrame.from_dict(history.history).to_csv('all_with_v1.csv', index=False)
+pd.DataFrame.from_dict(history.history).to_csv('increased_params_v4.csv', index=False)
     
 model.save(checkpoint_filepath)
